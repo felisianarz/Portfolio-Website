f917ba0ca6baf56efbfba8c9ba5c59ef8327040b
Felisiana Ardelia Azzahra <42663389+felisianarz@users.noreply.github.com>
2021-05-30 00:16:17 +0700

    Update home.php

--- home.php
+++ home.php
@@ -1,13 +1,22 @@
-<!-- function -->
+<!-- Class & Constructor -->
 <?php 
-function welcome(){
-	echo "Welcome to My Portfolio";
-}
+class home{
+      public $name;
+    
+      function __construct($name) {
+        $this->name = $name; 
+      }
+      function get_name() {
+        return $this->name;
+      }
+    }
+    
+    $porto = new home("Welcome to my portofolio");
 ?>
     
 <!-- Jumbotron -->
 <section class="jumbotron text-center">
       <img src="img/home.png" alt="Felisiana" width="700" />
-      <h1 ><?php welcome(); ?></h1>
+      <h1 ><?php echo $porto->get_name();  ?></h1>
 </section>
 <!-- Akhir Jumbrotron -->
